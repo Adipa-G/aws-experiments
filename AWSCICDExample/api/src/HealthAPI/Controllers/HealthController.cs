f03da1dd5af969f36fe2e4d4e4a13db04158f751
adipa <adipa.achntha@gmail.com>
2021-07-02 15:56:05 +1000

    adding the solution

---
+++ AWSCICDExample/api/src/HealthAPI/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthAPI.Controllers
+{
+    [Route("/api/health")]
+    public class HealthController
+    {
+        [Route("")]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return new ObjectResult(new { Healty = true });
+        }
+    }
+}

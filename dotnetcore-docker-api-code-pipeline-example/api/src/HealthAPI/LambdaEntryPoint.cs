4e6d512c2b2c0061e64025c37e3bc5d79f53575e
adipa <adipa.achntha@gmail.com>
2021-07-13 10:30:25 +1000

    testing docker build

---
+++ dotnetcore-docker-api-code-pipeline-example/api/src/HealthAPI/LambdaEntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+using Amazon.XRay.Recorder.Handlers.AwsSdk;
+using Microsoft.AspNetCore.Hosting;
+
+namespace HealthAPI
+{
+    public class LambdaEntryPoint : APIGatewayProxyFunction
+    {
+        static LambdaEntryPoint()
+        {
+            AWSSDKHandler.RegisterXRayForAllServices();
+        }
+
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseStartup<Startup>();
+        }
+    }
+}

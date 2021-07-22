4e322dcc87bdf3684ca17db793e239f4be58960d
adipa <adipa.achntha@gmail.com>
2021-07-22 11:47:42 +1000

    update format

--- dotnetcore-docker-api-with-dynamo-db-code-pipeline-example/api/src/HealthAPI/Controllers/HealthController.cs
+++ dotnetcore-docker-api-with-dynamo-db-code-pipeline-example/api/src/HealthAPI/Controllers/HealthController.cs
@@ -12,13 +12,13 @@ namespace HealthAPI.Controllers
         private readonly IAmazonDynamoDB _dynamoDb;
         private readonly ILogger<HealthController> _logger;
 
-        public HealthController(IAmazonDynamoDB dynamoDb, 
+        public HealthController(IAmazonDynamoDB dynamoDb,
             ILogger<HealthController> logger)
         {
             _dynamoDb = dynamoDb;
             _logger = logger;
         }
-        
+
         [Route("")]
         [HttpGet]
         public async Task<ActionResult> Index()

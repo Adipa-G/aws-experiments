41715e6b205dfbe4fd68539cc262f6669494fb7e
adipa <adipa.achntha@gmail.com>
2021-09-10 16:49:48 +1000

    make perf count

--- grails-multi-app-docker-code-pipeline-example/test-app-one/grails-app/controllers/test/app/one/HealthController.groovy
+++ grails-multi-app-docker-code-pipeline-example/test-app-one/grails-app/controllers/test/app/one/HealthController.groovy
@@ -1,10 +1,23 @@
 package test.app.one
 
+import java.util.Random
+
 class HealthController {
     def index() { 
-        sleep(2000) {
-            render(contentType: "application/json", text: "{\"healthy\":\"false\", \"version\":\"one\"}")                
+        int nThrows = 0
+        int nSuccess = 0
+        double x, y
+        long then = System.nanoTime()
+        int events=1e8
+        Random r = new Random()
+        for (int i = 0; i < events; i++) {
+                    x = r.nextFloat()      // Throw a dart
+                    y = r.nextFloat()
+                    nThrows++
+                    if ( x*x + y*y <= 1 )  nSuccess++
         }
-        render(contentType: "application/json", text: "{\"healthy\":\"false\", \"version\":\"one\"}")                
+        double itime = ((System.nanoTime() - then)/1e9)
+        
+        render(contentType: "application/json", text: "{\"healthy\":\"false\", \"version\":\"one\",\"time_to_calc\":\"" + itime + "\",\"pi\":\"" + 4*(double)nSuccess/(double)nThrows + "\"}")                
     }
 }

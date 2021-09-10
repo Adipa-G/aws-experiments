cd7fb9f50fd94f3c69fc1b699cb770ed400272b2
adipa <adipa.achntha@gmail.com>
2021-09-10 18:10:40 +1000

    roll back timeout

--- grails-multi-app-docker-code-pipeline-example/test-app-two/grails-app/controllers/test/app/two/HealthController.groovy
+++ grails-multi-app-docker-code-pipeline-example/test-app-two/grails-app/controllers/test/app/two/HealthController.groovy
@@ -1,23 +1,28 @@
 package test.app.two
 
-import java.util.Random
-
 class HealthController {
     def index() { 
-        int nThrows = 0
-        int nSuccess = 0
-        double x, y
-        long then = System.nanoTime()
-        int events=1e8
-        Random r = new Random()
-        for (int i = 0; i < events; i++) {
-                    x = r.nextFloat()      // Throw a dart
-                    y = r.nextFloat()
-                    nThrows++
-                    if ( x*x + y*y <= 1 )  nSuccess++
+        sleep(2000) {
+            render(contentType: "application/json", text: "{\"healthy\":\"false\", \"version\":\"two\"}")                
         }
-        double itime = ((System.nanoTime() - then)/1e9)
-        
-        render(contentType: "application/json", text: "{\"healthy\":\"false\", \"version\":\"one\",\"time_to_calc\":\"" + itime + "\",\"pi\":\"" + 4*(double)nSuccess/(double)nThrows + "\"}")                
+        render(contentType: "application/json", text: "{\"healthy\":\"false\", \"version\":\"two\"}")                
     }
+
+    // def index() { 
+    //     int nThrows = 0
+    //     int nSuccess = 0
+    //     double x, y
+    //     long then = System.nanoTime()
+    //     int events=1e8
+    //     Random r = new Random()
+    //     for (int i = 0; i < events; i++) {
+    //                 x = r.nextFloat()      // Throw a dart
+    //                 y = r.nextFloat()
+    //                 nThrows++
+    //                 if ( x*x + y*y <= 1 )  nSuccess++
+    //     }
+    //     double itime = ((System.nanoTime() - then)/1e9)
+        
+    //     render(contentType: "application/json", text: "{\"healthy\":\"false\", \"version\":\"one\",\"time_to_calc\":\"" + itime + "\",\"pi\":\"" + 4*(double)nSuccess/(double)nThrows + "\"}")                
+    // }
 }

285d2fd09ee5c7e55e4ca2f0663fb0b8f26517e8
adipa <adipa.achntha@gmail.com>
2021-09-13 10:16:25 +1000

    adding load controller

---
+++ grails-multi-app-docker-code-pipeline-example/test-app-two/grails-app/controllers/test/app/two/LoadController.groovy
@@ -0,0 +1,24 @@
+package test.app.two
+
+import java.util.Random
+
+class LoadController {
+    def index() { 
+        int nThrows = 0
+        int nSuccess = 0
+        double x, y
+        long then = System.nanoTime()
+        int events=1e8
+        Random r = new Random()
+        for (int i = 0; i < events; i++) {
+                    x = r.nextFloat()      // Throw a dart
+                    y = r.nextFloat()
+                    nThrows++
+                    if ( x*x + y*y <= 1 )  nSuccess++
+        }
+        double itime = ((System.nanoTime() - then)/1e9)
+        
+        render(contentType: "application/json", text: "{\"time_to_calc\":\"" + itime + "\",\"pi\":\"" + 4*(double)nSuccess/(double)nThrows + "\"}")                
+    }
+}
+

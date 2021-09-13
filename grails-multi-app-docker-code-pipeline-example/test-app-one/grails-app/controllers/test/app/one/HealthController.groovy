4855f9c1bbe2129f7f6f884c4a0776b93b902afd
adipa <adipa.achntha@gmail.com>
2021-09-13 10:29:57 +1000

    update health

--- grails-multi-app-docker-code-pipeline-example/test-app-one/grails-app/controllers/test/app/one/HealthController.groovy
+++ grails-multi-app-docker-code-pipeline-example/test-app-one/grails-app/controllers/test/app/one/HealthController.groovy
@@ -2,9 +2,6 @@ package test.app.one
 
 class HealthController {
     def index() { 
-        sleep(2000) {
-            render(contentType: "application/json", text: "{\"healthy\":\"false\", \"version\":\"two\"}")                
-        }
-        render(contentType: "application/json", text: "{\"healthy\":\"false\", \"version\":\"two\"}")                
+        render(contentType: "application/json", text: "{\"healthy\":\"false\", \"version\":\"one\"}")                
     }
 }
--- grails-multi-app-docker-code-pipeline-example/test-app-two/grails-app/controllers/test/app/two/HealthController.groovy
+++ grails-multi-app-docker-code-pipeline-example/test-app-two/grails-app/controllers/test/app/two/HealthController.groovy
@@ -2,27 +2,6 @@ package test.app.two
 
 class HealthController {
     def index() { 
-        sleep(2000) {
-            render(contentType: "application/json", text: "{\"healthy\":\"false\", \"version\":\"two\"}")                
-        }
         render(contentType: "application/json", text: "{\"healthy\":\"false\", \"version\":\"two\"}")                
     }
-
-    // def index() { 
-    //     int nThrows = 0
-    //     int nSuccess = 0
-    //     double x, y
-    //     long then = System.nanoTime()
-    //     int events=1e8
-    //     Random r = new Random()
-    //     for (int i = 0; i < events; i++) {
-    //                 x = r.nextFloat()      // Throw a dart
-    //                 y = r.nextFloat()
-    //                 nThrows++
-    //                 if ( x*x + y*y <= 1 )  nSuccess++
-    //     }
-    //     double itime = ((System.nanoTime() - then)/1e9)
-        
-    //     render(contentType: "application/json", text: "{\"healthy\":\"false\", \"version\":\"one\",\"time_to_calc\":\"" + itime + "\",\"pi\":\"" + 4*(double)nSuccess/(double)nThrows + "\"}")                
-    // }
 }

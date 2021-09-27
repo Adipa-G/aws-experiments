61a2db82650c5826fe1afadb9beadba3a1870c2c
adipa <adipa.achntha@sensis.com.au>
2021-09-27 15:53:58 +1000

    adding env files

--- grails-app-docker-code-pipeline-example/app/grails-app/controllers/test/app/HealthController.groovy
+++ grails-app-docker-code-pipeline-example/app/grails-app/controllers/test/app/HealthController.groovy
@@ -2,8 +2,9 @@ package test.app
 
 class HealthController {
     String appName = System.getenv("APP_NAME")
+    String appVersion = System.getenv("APP_VERSION")
     def index() { 
-        render(contentType: "application/json", text: "{\"healthy\":\"false\", \"app-name\":\"" + appName + "\"}")                
+        render(contentType: "application/json", text: "{\"healthy\":\"false\", \"app-name\":\"" + appName + "\", \"app-version\":\"" + appVersion + "\"}")                
     }
 }
 

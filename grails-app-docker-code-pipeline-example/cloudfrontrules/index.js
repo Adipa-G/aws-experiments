c0fadb3b643ad4d90684d0ce50ef27e908e74213
adipa <adipa.achntha@sensis.com.au>
2021-10-07 17:35:50 +1100

    separating out the rules

---
+++ grails-app-docker-code-pipeline-example/cloudfrontrules/index.js
@@ -0,0 +1,27 @@
+'use strict';
+const querystring = require('querystring');
+
+exports.handler = (event, context, callback) => {
+  const request = event.Records[0].cf.request;
+
+  const headers = request.headers;
+  const params = querystring.parse(request.querystring);
+
+  if (params.x){
+	const redirectUrl = `http://${headers.host[0].value}${request.uri}/${params.x}`;
+	const response = {
+		status: '302',
+		statusDescription: 'Found',
+		headers: {
+			location: [{
+				key: 'Location',
+				value: redirectUrl,
+			}],
+		},
+	};
+	callback(null, response);
+  }
+  else{
+	callback(null, request);
+  }
+};
